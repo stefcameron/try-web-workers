7c926448aeba605df8cdf6a788cbdb1278af26c1
Stefan Cameron <stefan@stefcameron.com>
2023-11-03 12:38:14 -0500

    Basic layout and demo switcher

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,62 @@
+import { useState } from 'react';
 import './App.styles.css';
 
+const demoTypes = Object.freeze({
+  SERVICE: 'serviceWorker',
+  SHARED: 'sharedWorker',
+  DEDICATED: 'dedicatedWorker',
+});
+
 export const App = function () {
+  const [demoType, setDemoType] = useState('serviceWorker');
+
+  const demoTitle =
+    demoType === demoTypes.SERVICE
+      ? 'Service Worker'
+      : demoType === demoTypes.SHARED
+      ? 'Shared Worker'
+      : 'Dedicated Worker';
+
   return (
     <div className="app">
-      <h1>React App</h1>
-      <h2>Template</h2>
+      <h1>Web Worker Demos</h1>
+      <div className="app__demos">
+        <div className="app__demos__selector">
+          <label>
+            <input
+              type="radio"
+              name="demoType"
+              value={demoTypes.SERVICE}
+              checked={demoType === demoTypes.SERVICE}
+              onChange={() => setDemoType(demoTypes.SERVICE)}
+            />
+            Service Worker
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="demoType"
+              value={demoTypes.SHARED}
+              checked={demoType === demoTypes.SHARED}
+              onChange={() => setDemoType(demoTypes.SHARED)}
+            />
+            Shared Worker
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="demoType"
+              value={demoTypes.DEDICATED}
+              checked={demoType === demoTypes.DEDICATED}
+              onChange={() => setDemoType(demoTypes.DEDICATED)}
+            />
+            Service Worker
+          </label>
+        </div>
+        <div className="app__demos__demo-box">
+          <h2>{demoTitle}</h2>
+        </div>
+      </div>
     </div>
   );
 };
